9df479de2e6bf58535e8edaa976a7009e1df346f
krystiano13 <crystianotv@gmail.com>
2023-07-27 11:22:45 +0200

    styles for mobile navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+import bag from "../assets/bag.svg";
+import search from "../assets/search.svg";
+
 const Navbar = () => {
   return (
-    <nav className="container-fluid d-flex align-items-center justify-content-between">
+    <nav className="d-flex align-items-center justify-content-between">
+      <button className="hamburgerMenu d-flex flex-column align-items-center justify-content-center d-lg-none">
+        <div id="firstStripe" className="stripe"></div>
+        <div className="stripe"></div>
+        <div id="lastStripe" className="stripe"></div>
+      </button>
       <div className="logo d-flex align-items-center justify-content-center">
         NS
       </div>
-      <div className="navigation d-flex align-items-center justify-content-center">
+      <div className="navigation d-lg-flex d-none align-items-center justify-content-center">
         <NavLink className="a" to="/">
           ALL PRODUCTS
         </NavLink>
@@ -23,8 +31,17 @@ const Navbar = () => {
         <NavLink className="a" to="/">
           COLLECTIONS
         </NavLink>
-        <button className="searchButton"></button>
-        <button className="cartButton"></button>
+        <button className="searchButton">
+          <img src={search} alt="search" />
+        </button>
+        <button className="cartButton">
+          <img src={bag} alt="bag" />
+        </button>
+      </div>
+      <div className="navigation d-flex d-lg-none align-items-center justify-content-end">
+        <button className="cartButton">
+          <img src={bag} alt="bag" />
+        </button>
       </div>
     </nav>
   );
