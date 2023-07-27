065d683a5a9efb84555acd84110318e8c7c8f8c9
krystiano13 <crystianotv@gmail.com>
2023-07-27 10:16:49 +0200

    configure bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = createRoot(document.querySelector("#root") as HTMLElement);
 
