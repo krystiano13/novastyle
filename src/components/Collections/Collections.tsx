4f4186a5bcd8d868b0debceb93ca31c61cf941ed
krystiano13 <crystianotv@gmail.com>
2023-07-28 11:38:59 +0200

    initial styles for hero slider

--- src/components/Collections/Collections.tsx
+++ src/components/Collections/Collections.tsx
@@ -1,19 +1,53 @@
 import React from "react";
 
 const Collections = () => {
-    return (
-      <div className="collections">
-        <div className="container-fluid">
-          <div className="row">
-            <button id="c1" className="col-md-12 d-flex flex-dolumn align-items-end">Modern Elegance</button>
-            <button id="c2" className="col-md-12 col-lg-6 d-flex flex-dolumn align-items-end">Women</button>
-            <button id="c3" className="col-md-12 col-lg-6 d-flex flex-dolumn align-items-end">Men</button>
-            <button id="c4" className="col-md-12 col-lg-6 d-flex flex-dolumn align-items-end">Accessories</button>
-            <button id="c5" className="col-md-12 col-lg-6 d-flex flex-dolumn align-items-end">Sale</button>
-          </div>
+  return (
+    <div className="collections">
+      <div className="container-fluid">
+        <div className="row">
+          <button className="col-md-12 d-flex flex-dolumn align-items-end">
+            <div id="c1" className="bg">
+              <div className="content">
+                <button>
+                  <p>Collection</p>
+                  <h2>Modern Elegance</h2>
+                </button>
+                <div className="dots">
+                  <button className="dot choosen"></button>
+                  <button className="dot"></button>
+                  <button className="dot"></button>
+                </div>
+              </div>
+            </div>
+          </button>
+          <button
+            id="c2"
+            className="col-md-12 col-lg-6 d-flex flex-dolumn align-items-end"
+          >
+            <span>Women</span>
+          </button>
+          <button
+            id="c3"
+            className="col-md-12 col-lg-6 d-flex flex-dolumn align-items-end"
+          >
+            <span>Men</span>
+          </button>
+          <button
+            id="c4"
+            className="col-md-12 col-lg-6 d-flex flex-dolumn align-items-end"
+          >
+            <span>Accessories</span>
+          </button>
+          <button
+            id="c5"
+            className="col-md-12 col-lg-6 d-flex flex-dolumn align-items-end"
+          >
+            <span>Sale</span>
+          </button>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export { Collections };
