b15960ea401a6ac7bdb7521c05562dbbe6943bbc
krystiano13 <crystianotv@gmail.com>
2023-07-28 11:07:03 +0200

    collections buttons

--- src/components/Collections/Collections.tsx
+++ src/components/Collections/Collections.tsx
@@ -5,11 +5,11 @@ const Collections = () => {
       <div className="collections">
         <div className="container-fluid">
           <div className="row">
-            <button className="col-md-12"></button>
-            <button className="col-md-12 col-lg-6"></button>
-            <button className="col-md-12 col-lg-6"></button>
-            <button className="col-md-12 col-lg-6"></button>
-            <button className="col-md-12 col-lg-6"></button>
+            <button id="c1" className="col-md-12 d-flex flex-dolumn align-items-end">Modern Elegance</button>
+            <button id="c2" className="col-md-12 col-lg-6 d-flex flex-dolumn align-items-end">Women</button>
+            <button id="c3" className="col-md-12 col-lg-6 d-flex flex-dolumn align-items-end">Men</button>
+            <button id="c4" className="col-md-12 col-lg-6 d-flex flex-dolumn align-items-end">Accessories</button>
+            <button id="c5" className="col-md-12 col-lg-6 d-flex flex-dolumn align-items-end">Sale</button>
           </div>
         </div>
       </div>
