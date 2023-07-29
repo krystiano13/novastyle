f61532324f040045a3848fb55f582690c87c2b3e
krystiano13 <crystianotv@gmail.com>
2023-07-29 11:00:48 +0200

    structure for bestsellers section

---
+++ src/components/Bestsellers/Bestsellers.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Bestsellers = () => {
+  return (
+    <section className="Bestsellers container-fluid">
+      <div className="row">
+        <div className="col-4">
+          <img src="" alt="" />
+          <p></p>
+        </div>
+        <div className="col-4">
+          <p className="lead">OUR</p>
+          <h2>Bestsellers</h2>
+        </div>
+        <div className="col-4">
+          <img src="" alt="" />
+          <p></p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-3">
+          <img src="" alt="" />
+        </div>
+        <div className="col-3">
+          <img src="" alt="" />
+        </div>
+        <div className="col-3">
+          <img src="" alt="" />
+        </div>
+        <div className="col-3">
+          <img src="" alt="" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export { Bestsellers };
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Collections } from "../components/Collections/Collections";
+import { Bestsellers } from "../components/Bestsellers/Bestsellers";
 
 const Home = () => {
   return (
     <>
       <Collections />
+      <Bestsellers />
     </>
   );
 };
