e03cf1cb39515618cc53a926c3fb8d29c6efa5ca
krystiano13 <crystianotv@gmail.com>
2023-07-31 11:01:50 +0200

    structure for footer newsletter form

---
+++ src/components/Footer/BeFirst.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const BeFirst = () => {
+  return (
+    <form className="col-6">
+      <h2>Be First</h2>
+      <input placeholder="Email To Subscribe" type="email" />
+      <button type="submit">SUBSCRIBE</button>
+      <p>Private sales, new arrivals and seasonal.</p>
+    </form>
+  );
+};
+
+export { BeFirst };
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import { FooterAbout } from "./FooterAbout";
 import { FooterNavigation } from "./FooterNavigation";
+import { BeFirst } from "./BeFirst";
 
 const Footer = () => {
     return (
         <footer className="container-fluid row">
             <FooterAbout />
             <FooterNavigation />
+            <BeFirst />
         </footer>
     )
 }
