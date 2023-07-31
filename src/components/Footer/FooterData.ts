a21978d067b8e8107255e4416dfcd7d4bba314c4
krystiano13 <crystianotv@gmail.com>
2023-07-31 10:59:13 +0200

    structure for Footer navigation section

---
+++ src/components/Footer/FooterData.ts
@@ -0,0 +1,53 @@
+import n1 from '../../assets/footer/n1.svg';
+import n2 from "../../assets/footer/n2.svg";
+import n3 from "../../assets/footer/n3.svg";
+import n4 from "../../assets/footer/n4.svg";
+import n5 from "../../assets/footer/n5.svg";
+import n6 from "../../assets/footer/n6.svg";
+import n7 from "../../assets/footer/n7.svg";
+import n8 from "../../assets/footer/n8.svg";
+
+const footerNav = [
+  {
+    id: 0,
+    title: "Start Page",
+    src: n1,
+  },
+  {
+    id: 1,
+    title: "Home",
+    src: n2,
+  },
+  {
+    id: 2,
+    title: "Products",
+    src: n3,
+  },
+  {
+    id: 3,
+    title: "About Us",
+    src: n4,
+  },
+  {
+    id: 4,
+    title: "Contact Us",
+    src: n5,
+  },
+  {
+    id: 5,
+    title: "Questions",
+    src: n6,
+  },
+  {
+    id: 6,
+    title: "Collections",
+    src: n7,
+  },
+  {
+    id: 7,
+    title: "Privacy",
+    src: n8,
+  },
+];
+
+export { footerNav };
--- src/components/Footer/FooterNavigation.tsx
+++ src/components/Footer/FooterNavigation.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 
+import { FooterButton } from "./FooterButton";
+import { footerNav } from "./FooterData";
+
 const FooterNavigation = () => {
-    return (
-        <div className="col-6"></div>
-    )
-}
+  return (
+    <div className="col-6">
+      <h2>Navigation</h2>
+      <div className="buttons">
+        {footerNav.map((item) => (
+          <FooterButton key={item.id} src={item.src} title={item.title} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export { FooterNavigation };
