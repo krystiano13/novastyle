8a7a586c041ec7be6f738e8b44f31131b6d61a68
krystiano13 <crystianotv@gmail.com>
2023-08-01 10:27:06 +0200

    footer styles for desktop

--- src/components/Footer/BeFirst.tsx
+++ src/components/Footer/BeFirst.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BeFirst = () => {
   return (
-    <section className="col-lg-6 col-md-12">
+    <section id="BeFirst" className="col-lg-6 col-md-12">
       <h2>Be First</h2>
       <form>
         <input placeholder="Email To Subscribe" type="email" />
