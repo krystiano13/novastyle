ac9a5e9b0bac9e09b77927afdfbf2fef3d175981
krystiano13 <crystianotv@gmail.com>
2023-08-02 09:48:47 +0200

    assets and form styles for contact page

--- src/views/Contact.tsx
+++ src/views/Contact.tsx
@@ -11,32 +11,32 @@ const Contact = () => {
       <h2>Contact Info</h2>
       <div className="row">
         <div className="col-md-12 col-lg-4">
-          <img src={cn1} alt="model" />
+          <img className="img" src={cn1} alt="model" />
         </div>
-        <form className="col-md-12 col-lg-4">
+        <form className="col-md-12 col-lg-4 d-flex flex-column justify-content-center align-items-center">
           <input type="text" placeholder="Name" />
           <input type="email" placeholder="Email" />
           <textarea placeholder="Message ..." />
           <button type="submit">Send</button>
         </form>
         <div className="col-md-12 col-lg-4">
-          <img src={cn2} alt="model" />
+          <img className="img" src={cn2} alt="model" />
         </div>
         <div className="col-md-12 col-lg-4">
-          <img src="" alt="" />
+          <img className="icon" src="" alt="icon" />
           <p></p>
           <p></p>
-          <img src="" alt="" />
+          <img className="icon" src="" alt="icon" />
           <h3></h3>
         </div>
         <div className="col-md-12 col-lg-4">
-          <img src={ab3} alt="model" />
+          <img className="img" src={ab3} alt="model" />
         </div>
         <div className="col-md-12 col-lg-4">
-          <img src="" alt="" />
+          <img className="icon" src="" alt="icon" />
           <p></p>
           <p></p>
-          <img src="" alt="" />
+          <img className="icon" src="" alt="icon" />
           <h3></h3>
         </div>
       </div>
