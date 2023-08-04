15291fc47960d45f7a2e8f9b0930c0b6cc87409e
krystiano13 <crystianotv@gmail.com>
2023-08-04 10:20:35 +0200

    structure and bootstrap styles for faq page

--- src/views/Questions.tsx
+++ src/views/Questions.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from "react";
+
+import { QuestionsData } from "../components/Questions/QuestionsData";
 
 const Questions = () => {
-    return (
-        <main className='Questions container-fluid'>
-            <div className='row'>
-                
-            </div>
-        </main>
-    )
-}
+  return (
+    <main className="Questions container-fluid">
+      <div className="row">
+        {QuestionsData.map((item) => (
+          <div key={item.id} className="col-lg-3 col-md-12">
+            {item.isImage && <img src={item.imgSrc} alt="model" />}
+            {item.isVid && (
+              <video loop muted autoPlay src={item.imgSrc}></video>
+            )}
+            {item.isImage === false && item.isVid === false ? (
+              <React.Fragment>
+                <h3>{item.title}</h3>
+                <p>{item.content}</p>
+              </React.Fragment>
+            ) : null}
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+};
 
 export { Questions };
