f844a3260eb2dc97e9b69fe51231c1b774a3250c
krystiano13 <crystianotv@gmail.com>
2023-08-06 12:46:57 +0200

    styles for search view

--- src/views/Search.tsx
+++ src/views/Search.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Search = () => {
-    return (
-        <form className='search container-fluid'>
-            <input type='text' placeholder='Example: bright blue summer shirt' />
-            <button className='btn' type='submit'>Search</button>
-        </form>
-    )
-}
+  return (
+    <form className="search container-fluid d-flex flex-column justify-content-center">
+      <input type="text" placeholder="Example: bright blue summer shirt" />
+      <button className="btn" type="submit">
+        Search
+      </button>
+    </form>
+  );
+};
 
 export { Search };
