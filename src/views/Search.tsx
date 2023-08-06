1f7ee4ace0cf3258ad369a9a3ea1bdac68fe39c0
krystiano13 <crystianotv@gmail.com>
2023-08-06 12:28:24 +0200

    structure for search view

---
+++ src/views/Search.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Search = () => {
+    return (
+        <form className='search container-fluid'>
+            <input type='text' placeholder='Example: bright blue summer shirt' />
+            <button className='btn' type='submit'>Search</button>
+        </form>
+    )
+}
