357fa1fec6179a02ec2ec0cd4f5aace483dd493f
krystiano13 <crystianotv@gmail.com>
2023-08-07 10:10:12 +0200

    modal component structure

---
+++ src/components/Navbar/Modal.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Modal = () => {
+    return (
+      <div className="cart_modal">
+        <div className="modal_content">
+          <div className="modal_bar">
+            <p>Your Cart</p>
+            <button className="modal_btn">
+              <div className="modal_btn_stripe"></div>
+              <div className="modal_btn_stripe"></div>
+            </button>
+          </div>
+          <div>
+            <p className="lead">No items found.</p>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export { Modal };
